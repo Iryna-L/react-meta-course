79e06a1c7656ed18b83c89f1e1d4c3080b23b86f
ira <iryna.volkotrub@cd.p2h.com>
2024-03-25 14:39:55 +0200

    refactor booking

--- src/App.js
+++ src/App.js
@@ -5,8 +5,28 @@ import {
 } from "react-router-dom";
 import HomePage from "./components/Home"
 import BookingPage from "./components/Booking/BookingPage"
+import { useReducer } from 'react'
 
-const router = createBrowserRouter([
+function App() {
+  function initializeTimes() {
+    return {
+      times: ['16:00', '17:00', '18:00', '19:00', '20:00'],
+    };
+  }
+
+  function reducer(state, action) {
+    const newTimes = ['16:00', '17:00', '18:00', '19:00', '20:00']
+    return { times: [...newTimes] };
+  }
+
+  function submitForm(formData) {
+    console.log('submit', formData)
+  }
+
+  const initialState = initializeTimes();
+  const [availableTimes, setAvailableTimes] = useReducer(reducer, initialState);
+
+  const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
@@ -33,11 +53,14 @@ const router = createBrowserRouter([
   },
   {
     path: "booking",
-    element: <BookingPage />,
+    element: <BookingPage
+      availableTimes={availableTimes}
+      setAvailableTimes={setAvailableTimes}
+      submitForm={submitForm}
+    />,
   }
 ]);
 
-function App() {
   return (
     <div className="App">
       <RouterProvider router={router}/>
